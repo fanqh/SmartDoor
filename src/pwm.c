c5e579a3192414c94380865815400c9cedc813c6
fanqh <fqh1100@163.com>
2016-03-16 21:27:08 +0800

    touch beep is ok

--- src/pwm.c
+++ src/pwm.c
@@ -31,7 +31,7 @@ void Beep_PWM_TimeBase_config(uint16_t period)
 		  /* Set the default configuration */
   TIM_TimeBaseInitStruct.TIM_Period = period;//100-1;//370-1; //300-1//450
   TIM_TimeBaseInitStruct.TIM_Prescaler =48-1; //480 -1;  //1us
-  TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV2;
   TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
 //  TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0x0000;
 	TIM_TimeBaseInit(TIM16, &TIM_TimeBaseInitStruct);
@@ -210,9 +210,9 @@ void Beep_Four_Time(void)
 
 void Key_Touch_Beep_Warm(void)
 {
-	Beep_PWM_TimeBase_config(360); //420
-	Beep_PWM_config(130);       //200	  
-	Hal_Beep_Blink (1, 35, 10);	
+	Beep_PWM_TimeBase_config(370); //420  //360 //500
+	Beep_PWM_config(100);          //220 //130  //230
+	Hal_Beep_Blink (1, 40, 10);	
 }
 
 void Beep_Compare_ID_Err(void)
